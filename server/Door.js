ae615690a57b9d0130d688662f98204b83842572
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-07-05 00:47:28 +0200

    Added phone app

---
+++ server/Door.js
@@ -0,0 +1,16 @@
+var rpio = require('rpio');
+var sleep = require('sleep');
+
+var config = require('./Config');
+
+function Door () {
+    rpio.open(config.pin.action, rpio.OUTPUT, rpio.LOW);
+}
+
+Door.prototype.action = function () {
+    rpio.write(config.pin.action, rpio.HIGH);
+    sleep.msleep(150);
+    rpio.write(config.pin.action, rpio.LOW);
+}
+
+module.exports = Door;
